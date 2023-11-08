53c1651b81d7bf38871bcd73039e1c88e6e5bd48
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2023-11-08 14:42:09 +0000

    Star Wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,28 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
 
-function getMovieCharacters(movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-  request.get(filmsUrl, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
-
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (error, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
-  });
-}
-
-function printCharacters(characters, movieTitle) {
-  console.log(`Characters in ${movieTitle}:`);
-  for (const character of characters) {
-    console.log(character);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
   }
-}
-
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
-}
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+  }
+  );
+  Promise.all(promises).then((names) => {
+    names.forEach((name) => {
+      console.log(name);
+    });
+  }
+  );
+});
